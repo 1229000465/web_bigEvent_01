19386eee0cbb315c226329c3c70ddecdd47fbf72
taotao <1229000465@qq.com>
2020-10-08 15:56:28 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -6,4 +6,23 @@ var baseURL = "http://ajax.frontend.itheima.net";
 $.ajaxPrefilter(function(params) {
     //拼接对应环境的服务器地址
     params.url = baseURL + params.url;
+
+    //对需要权限的接口配置头信息
+    //必须以my开头
+    if (params.url.indexOf("/my/") !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+
+    //拦截所有响应，判断身份认证信息
+    params.complete = function(res) {
+        console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //清空本地token
+            localStorage.removeItem("token");
+            //页面跳转
+            location.href = "/login.html"
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,56 @@
+$(function() {
+    getUserInof();
+
+    //退出
+    var layer = layui.layer;
+    $("#btnLongout").on("click", function() {
+        layer.confirm('是否确认退出', { icon: 3, title: '提示' }, function(index) {
+            localStorage.removeItem("token");
+            location.href = "/login.html"
+
+            layer.close(index);
+        })
+    })
+});
+
+function getUserInof() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem("token") || ""
+        // },
+        success: function(res) {
+            console.log(res);
+            if (res.status !== 0) {
+                return layui.layer.msg(res.message);
+            }
+            renderAvatar(res.data);
+        },
+        // complete: function(res) {
+        //     console.log(res);
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+        //         //清空本地token
+        //         localStorage.removeItem("token");
+        //         //页面跳转
+        //         location.href = "/login.html"
+        //     }
+        // }
+    })
+}
+
+// 封装用户头像渲染函数
+function renderAvatar(user) {
+    var name = user.nickname || user.username;
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+    if (user.user_pic !== null) {
+        //有头像
+        $(".layui-nav-img").show().attr("src", user.user_pic);
+        $(".user-avatar").hide();
+    } else {
+        //没头像
+        $(".layui-nav-img").hide();
+        var text = name[0].toUpperCase();
+        $(".user-avatar").show().html(text);
+    }
+}
